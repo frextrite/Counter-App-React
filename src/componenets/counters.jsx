f208a3b351cf4c23ceb668e043cf32ce032d052b
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-08-18 14:15:30 +0530

    Add reset button

--- src/componenets/counters.jsx
+++ src/componenets/counters.jsx
@@ -15,11 +15,12 @@ class Counters extends Component {
     console.log('Delete event called!', counterId);
     const counters = this.state.counters.filter(c => c.id != counterId);
     this.setState({ counters });
-  }
+  };
 
   render() {
     return (
       <div>
+        <button className="btn btn-primary btn-sm m-2" onReset={this.handleReset} >Reset</button>
         {this.state.counters.map(counter => (
           <Counter key={counter.id} value={counter.value} id={counter.id} onDelete={this.handleDelete} >
           </Counter>
